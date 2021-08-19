f5b51536cb74f8bd237122b475dc7d37d9ee8aa4
pigeonligh <pigeonligh@hotmail.com>
2021-08-19 14:02:43 +0800

    fix cli/flags

--- cli/flags/flags.go
+++ cli/flags/flags.go
@@ -36,64 +36,64 @@ func ObjectVar(fs *pflag.FlagSet, ptr interface{}, prefix string) error {
 		fieldValue := ref.Field(i)
 		name := prefix + fieldType.Tag.Get("name")
 		help := fieldType.Tag.Get("help")
-		shorthand := fieldType.Tag.Get("shorthand")
+		short := fieldType.Tag.Get("short")
 		if len(prefix) > 0 {
-			shorthand = ""
+			short = ""
 		}
 		fieldprefix := prefix + fieldType.Tag.Get("prefix")
 
 		obj := fieldValue.Addr().Interface()
 		switch ptr := obj.(type) {
 		case *bool:
-			fs.BoolVarP(ptr, name, shorthand, *ptr, help)
+			fs.BoolVarP(ptr, name, short, *ptr, help)
 
 		case *int:
-			fs.IntVarP(ptr, name, shorthand, *ptr, help)
+			fs.IntVarP(ptr, name, short, *ptr, help)
 		case *[]int:
-			fs.IntSliceVarP(ptr, name, shorthand, *ptr, help)
+			fs.IntSliceVarP(ptr, name, short, *ptr, help)
 		case *int32:
-			fs.Int32VarP(ptr, name, shorthand, *ptr, help)
+			fs.Int32VarP(ptr, name, short, *ptr, help)
 		case *[]int32:
-			fs.Int32SliceVarP(ptr, name, shorthand, *ptr, help)
+			fs.Int32SliceVarP(ptr, name, short, *ptr, help)
 		case *int64:
-			fs.Int64VarP(ptr, name, shorthand, *ptr, help)
+			fs.Int64VarP(ptr, name, short, *ptr, help)
 		case *[]int64:
-			fs.Int64SliceVarP(ptr, name, shorthand, *ptr, help)
+			fs.Int64SliceVarP(ptr, name, short, *ptr, help)
 
 		case *uint:
-			fs.UintVarP(ptr, name, shorthand, *ptr, help)
+			fs.UintVarP(ptr, name, short, *ptr, help)
 		case *[]uint:
-			fs.UintSliceVarP(ptr, name, shorthand, *ptr, help)
+			fs.UintSliceVarP(ptr, name, short, *ptr, help)
 		case *uint32:
-			fs.Uint32VarP(ptr, name, shorthand, *ptr, help)
+			fs.Uint32VarP(ptr, name, short, *ptr, help)
 		case *uint64:
-			fs.Uint64VarP(ptr, name, shorthand, *ptr, help)
+			fs.Uint64VarP(ptr, name, short, *ptr, help)
 
 		case *float32:
-			fs.Float32VarP(ptr, name, shorthand, *ptr, help)
+			fs.Float32VarP(ptr, name, short, *ptr, help)
 		case *[]float32:
-			fs.Float32SliceVarP(ptr, name, shorthand, *ptr, help)
+			fs.Float32SliceVarP(ptr, name, short, *ptr, help)
 		case *[]float64:
-			fs.Float64SliceVarP(ptr, name, shorthand, *ptr, help)
+			fs.Float64SliceVarP(ptr, name, short, *ptr, help)
 
 		case *time.Duration:
-			fs.DurationVarP(ptr, name, shorthand, *ptr, help)
+			fs.DurationVarP(ptr, name, short, *ptr, help)
 		case *[]time.Duration:
-			fs.DurationSliceVarP(ptr, name, shorthand, *ptr, help)
+			fs.DurationSliceVarP(ptr, name, short, *ptr, help)
 
 		case *string:
-			fs.StringVarP(ptr, name, shorthand, *ptr, help)
+			fs.StringVarP(ptr, name, short, *ptr, help)
 		case *[]string:
-			fs.StringSliceVarP(ptr, name, shorthand, *ptr, help)
+			fs.StringSliceVarP(ptr, name, short, *ptr, help)
 
 		case *net.IP:
-			fs.IPVarP(ptr, name, shorthand, *ptr, help)
+			fs.IPVarP(ptr, name, short, *ptr, help)
 		case *[]net.IP:
-			fs.IPSliceVarP(ptr, name, shorthand, *ptr, help)
+			fs.IPSliceVarP(ptr, name, short, *ptr, help)
 		case *net.IPMask:
-			fs.IPMaskVarP(ptr, name, shorthand, *ptr, help)
+			fs.IPMaskVarP(ptr, name, short, *ptr, help)
 		case *net.IPNet:
-			fs.IPNetVarP(ptr, name, shorthand, *ptr, help)
+			fs.IPNetVarP(ptr, name, short, *ptr, help)
 
 		default:
 			return ObjectVar(fs, ptr, fieldprefix)
